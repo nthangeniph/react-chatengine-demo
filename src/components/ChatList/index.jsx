2851a03ae51f25db84226c06dbe6030e296762bd
nthangeniphumudzo <60381173+nthangeniphumudzo@users.noreply.github.com>
2021-11-22 19:17:24 +0200

    media styling in progress

--- src/components/ChatList/index.jsx
+++ src/components/ChatList/index.jsx
@@ -1,49 +1,64 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 import { useChat } from 'components/context';
 import { Icon } from 'semantic-ui-react';
 import { notMe, joinUsernames } from 'helpers';
 import { ChatAvatar } from 'components';
+
+import { isTyping } from 'react-chat-engine';
+
+
 export const ChatList = () => {
-    const { myChats, chatconfig, selectedChat, selectedChatClick, deleteChatClick } = useChat();
+    const { myChats, chatConfig, selectedChat, selectedChatClick, deleteChatClick } = useChat();
+
+
+    if (myChats.length && chatConfig) {
+        console.log('checking here',chatConfig);
+        isTyping(chatConfig, myChats[0]?.id, (data) => console.log(data))
+}
+
+    useEffect(() => {
+        console.log('checking here',chatConfig);
+    }, [chatConfig])
+
     return (
-        <div className='chat-list'>
+        <div className='chat-list' style={{width:'100%'}}>
             {myChats.map((chat, index) => (
-                <div className={`chat-list ${selectedChat?.id === chat.id ? 'selected-chat-item' : ''}`} key={index}>
+                <div className={`chat-list ${selectedChat?.id === chat.id ? 'selected-chat-item' : ''}`} style={{display:'grid',gridTemplateColumns:'85% 15%'}} key={index}>
                     <div className='chat-list-item-content' onClick={() => selectedChatClick(chat)} >
                         {chat?.people?.length === 1 ? (
-                            <>
-                                <Icon circular inverted color='violet' name='use cancel' />
+                            <div className='icon-with-names' >
+                                <Icon circular inverted color='violet' name='user x' />
                                 <div className='chat-list-preview'>
                                     <div className='preview-username'>No one Added Yet</div>
                                 </div>
-                            </>
+                            </div>
                         ) : chat.people?.length === 2 ? (
-                                <>
-                                    <ChatAvatar username={notMe(chatconfig, chat)} chat={chat} />
+                            <div className='icon-with-names' >
+                                    <ChatAvatar username={notMe(chatConfig, chat)} chat={chat} />
                                     <div className='chat-list-preview'>
-                                        <div className='preview-username'>{notMe(chatconfig,chat)}</div>
+                                        <div className='preview-username'>{notMe(chatConfig,chat)}</div>
                                         <div className='preview-message'>
-                                          {chat.last_message.attachments.length?`${chat.last_message.sender.username} sent an attachment`:chat.last_message.text.slice(0,50)+ '....' }  
+                                          {chat.last_message.attachments.length?`${chat.last_message.sender.username} sent an attachment`:chat.last_message.text.slice(0,25)+ '....' }  
                                         </div>
                                     </div>
-                                </>
+                                </div>
                         ):
-                                <>
+                        <div className='icon-with-names' >
                                     <Icon circular inverted color='brown' name='users' />
                                     <div className='preview-username'>
-                                      {joinUsernames(chat?.people,chatconfig?.userName).slice(0,50) + '...'}
+                                      {joinUsernames(chat?.people,chatConfig?.userName).slice(0,25) + '...'}
                                     </div>
                                     <div className='preview-message'>
                                         {
-                                           chat.last_message.attachments.length?`${chat.last_message.sender.username} sent an attachment`:chat.last_message.text.slice(0,50) + '....' 
+                                           chat.last_message.attachments.length?`${chat.last_message.sender.username} sent an attachment`:chat.last_message.text.slice(0,25) + '....' 
                                         }
                                     </div>
-                                </>      
+                                </div>      
                         
                         }
                     </div>
                     <div onClick={() => deleteChatClick(chat)} className='chat-item-delete'>
-                        <Icon name='delete' />
+                    <Icon name='trash' />
                     </div>
                 </div>
             ))}
--- src/service/firebase.js
+++ src/service/firebase.js
@@ -11,10 +11,9 @@ try {
     projectId: process.env.REACT_APP_PROJECT_ID,
     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID
+    appId: process.env.REACT_APP_APP_ID,
   });
 } catch (error) {
-    debugger;
   if (!/already exists/u.test(error.message)) {
     console.error('Firebase admin initialization error', error.stack);
   }
@@ -29,6 +28,5 @@ export const useFirebase = () => {
     auth: firebase.auth(),
     storage: firebase.storage(),
     firestore: firebase.firestore(),
-  }
-
+  };
 };
